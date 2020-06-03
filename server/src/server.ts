5582a1db43a7cd410050b5d09a104772faef8958
Katriel Koga <katriel.koga.2012@hotmail.com>
2020-06-02 22:02:01 -0300

    Primeira Metade Dia 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
-
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', () => {
-    console.log('Listagem de usuarios')
-})
+app.use(express.json())
+app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
 
 app.listen(3333)
